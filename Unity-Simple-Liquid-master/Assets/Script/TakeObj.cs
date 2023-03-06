94230d76f0d7508939f866524729ebc4d0b9b8d8
VladNaboka <naboka.wlad@mail.ru>
2023-03-06 22:32:25 +0600

    Init Commit

---
+++ Unity-Simple-Liquid-master/Assets/Script/TakeObj.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeObj : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    private void Update()
+    {
+
+    }
+}
